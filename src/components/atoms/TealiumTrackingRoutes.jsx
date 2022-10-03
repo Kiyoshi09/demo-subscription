66f1686a6e39985723d2eb545120c026225cdb18
Kiyoshi09 <8742202+Kiyoshi09@users.noreply.github.com>
2022-10-03 17:41:07 +0900

    start implementing Tealium

---
+++ src/components/atoms/TealiumTrackingRoutes.jsx
@@ -0,0 +1,26 @@
+/* eslint react-hooks/exhaustive-deps: 0 */
+import App from "../../App";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
+import { Registration } from "../../pages/Registration";
+import { SignIn } from "../../pages/SignIn";
+import { MovieMain } from "../../pages/MovieMain";
+
+export const TealiumTrackingRoutes = () => {
+  let location = useLocation();
+
+  useEffect(() => {
+    window.utag &&
+        window.utag.view({"tealium_event": "screen_view", "page_path": location.pathname});
+  }, [location])
+
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="registration" element={<Registration />} />
+      <Route path="signin" element={<SignIn />} />
+      <Route path="moviemain" element={<MovieMain />} />
+    </Routes>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-import App from "./App";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import { Registration } from "./pages/Registration";
-import { SignIn } from "./pages/SignIn";
-import { MovieMain } from "./pages/MovieMain";
+// import App from "./App";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { Registration } from "./pages/Registration";
+// import { SignIn } from "./pages/SignIn";
+// import { MovieMain } from "./pages/MovieMain";
 
 import { Amplify } from "aws-amplify";
 import config from "./aws-exports";
+import { TealiumTrackingRoutes } from "./components/atoms/TealiumTrackingRoutes";
 
 Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="registration" element={<Registration />} />
-      <Route path="signin" element={<SignIn />} />
-      <Route path="moviemain" element={<MovieMain />} />
-    </Routes>
+    <TealiumTrackingRoutes />
+      {/* <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="registration" element={<Registration />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="moviemain" element={<MovieMain />} />
+      </Routes> */}
   </BrowserRouter>
 );
