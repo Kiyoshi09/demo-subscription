5ee4ebc5202488558f366f111e5e69251df01645
Kiyoshi09 <8742202+Kiyoshi09@users.noreply.github.com>
2023-03-15 23:32:26 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
+import { TealiumTrackingRoutes } from "./components/atoms/TealiumTrackingRoutes";
 
 import { Amplify } from "aws-amplify";
 import config from "./aws-exports";
-import { TealiumTrackingRoutes } from "./components/atoms/TealiumTrackingRoutes";
+
+import "./index.scss";
 
 Amplify.configure(config);
 
